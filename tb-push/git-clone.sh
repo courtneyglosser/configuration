81a4ba1adc1b3420fc38d07483827d33e88a9d92
Courtney Glosser <glosser_courtney@bah.com>
2015-06-30 11:41:03 -0400

    Git clone and update scripts for push dir

---
+++ tb-push/git-clone.sh
@@ -0,0 +1,6 @@
+
+
+git clone ssh://git@stash.csn.internal:7999/threatbase/frontend.git
+git clone ssh://git@stash.csn.internal:7999/threatbase/tier1.git
+git clone ssh://git@stash.csn.internal:7999/threatbase/infrastructure.git
+git clone ssh://git@stash.csn.internal:7999/threatbase/webserver.git
